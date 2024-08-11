d6e8f74fffd8c9320387b17bc2e4d41a51c2676e
Lucas-Winicius <lucasclucas7@gmail.com>
2024-08-11 01:09:10 -0300

    :sparkles: feat: Header Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <main>Blog</main>
+    <main></main>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import { Github, Linkedin, User } from "lucide-react";
+import { Separator } from "./ui/separator";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between my-2 mx-4 ">
+      <h1 className="text-3xl font-bold">Blog</h1>
+
+      <ul className="flex flex-row justify-between space-x-4 items-center font-semibold">
+        <li>
+          <a href="#">Início</a>
+        </li>
+        <li>
+          <a href="#">Artigos</a>
+        </li>
+        <li>
+          <a href="#">Sobre</a>
+        </li>
+      </ul>
+
+      <div className="flex justify-between items-center space-x-4">
+        <a href="#">
+          <Github size={20} />
+        </a>
+        <a href="#">
+          <Linkedin size={20} />
+        </a>
+        <Separator orientation="vertical" />
+        <button className="flex space-x-2 items-center">
+          <User size={20} />
+          <p className="text-sm font-medium">Sign in</p>
+        </button>
+      </div>
+    </header>
+  );
+}
