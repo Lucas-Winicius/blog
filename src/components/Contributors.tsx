bc3dc07d6dd0b316190a5524c10f2b43013f8823
Lucas-Winicius <lucasclucas7@gmail.com>
2024-08-11 23:52:26 -0300

    :sparkles: feat: Contributors Component

---
+++ src/components/Contributors.tsx
@@ -0,0 +1,54 @@
+import { CircleUserRound, UserRound } from "lucide-react";
+
+export default function Contributors() {
+  return (
+    <aside className="bg-zinc-100 px-4 py-6 space-y-5 rounded-lg w-72">
+      <h1 className="font-bold text-lg">Contribuidores</h1>
+      <div className="flex items-center space-x-4">
+        <div className="bg-stone-300 p-2 rounded-full border border-neutral-400">
+          <UserRound size={30} strokeWidth={1} />
+        </div>
+        <div>
+          <p className="font-bold">Lucas Winicius</p>
+          <p className="text-sm font-medium">lucas.winicius</p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <div className="bg-stone-300 p-2 rounded-full border border-neutral-400">
+          <UserRound size={30} strokeWidth={1} />
+        </div>
+        <div>
+          <p className="font-bold">Lucas Winicius</p>
+          <p className="text-sm font-medium">lucas.winicius</p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <div className="bg-stone-300 p-2 rounded-full border border-neutral-400">
+          <UserRound size={30} strokeWidth={1} />
+        </div>
+        <div>
+          <p className="font-bold">Lucas Winicius</p>
+          <p className="text-sm font-medium">lucas.winicius</p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <div className="bg-stone-300 p-2 rounded-full border border-neutral-400">
+          <UserRound size={30} strokeWidth={1} />
+        </div>
+        <div>
+          <p className="font-bold">Lucas Winicius</p>
+          <p className="text-sm font-medium">lucas.winicius</p>
+        </div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <div className="bg-stone-300 p-2 rounded-full border border-neutral-400">
+          <UserRound size={30} strokeWidth={1} />
+        </div>
+        <div>
+          <p className="font-bold">Lucas Winicius</p>
+          <p className="text-sm font-medium">lucas.winicius</p>
+        </div>
+      </div>
+    </aside>
+  );
+}
