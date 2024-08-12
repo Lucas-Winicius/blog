a9dedab58fa82bf93bb276e1e50cc96cf3684261
Lucas-Winicius <lucasclucas7@gmail.com>
2024-08-11 23:36:30 -0300

    :sparkles: feat: Recommendations Component

---
+++ src/components/Recommendations.tsx
@@ -0,0 +1,37 @@
+export default function Recommendations() {
+  return (
+    <aside className="bg-zinc-100 px-4 py-6 space-y-5 rounded-lg w-72">
+      <h1 className="font-bold text-lg">Recomendados</h1>
+      <div className="flex items-center space-x-5">
+        <h1 className="text-neutral-500 font-medium text-5xl">1</h1>
+        <p className="text-sm font-semibold">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+      <div className="flex items-center space-x-5">
+        <h1 className="text-neutral-500 font-medium text-5xl">2</h1>
+        <p className="text-sm font-semibold">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+      <div className="flex items-center space-x-5">
+        <h1 className="text-neutral-500 font-medium text-5xl">3</h1>
+        <p className="text-sm font-semibold">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+      <div className="flex items-center space-x-5">
+        <h1 className="text-neutral-500 font-medium text-5xl">4</h1>
+        <p className="text-sm font-semibold">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+      <div className="flex items-center space-x-5">
+        <h1 className="text-neutral-500 font-medium text-5xl">5</h1>
+        <p className="text-sm font-semibold">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+    </aside>
+  );
+}
