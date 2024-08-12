f0b021549bd5ca5f41be42b0e6052024aa26bc10
Lucas-Winicius <lucasclucas7@gmail.com>
2024-08-11 22:16:20 -0300

    :sparkles: feat: Search Component

---
+++ src/components/Search.tsx
@@ -0,0 +1,12 @@
+import { Search } from "lucide-react";
+
+export default function SearchComponent() {
+  return (
+    <form className="bg-zinc-100 max-w-fit flex items-center space-x-4 py-2 px-4 rounded-md">
+      <input type="search" name="search" className="bg-transparent outline-none" placeholder="TypeScript" />
+      <button type="submit">
+        <Search size={20} />
+      </button>
+    </form>
+  );
+}
