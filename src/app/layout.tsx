e61aee37b5361c694d99eb45fb5b60237a5b38df
Lucas-Winicius <lucasclucas7@gmail.com>
2024-08-21 22:51:32 -0300

    :sparkles: feat: Adding request to recommendations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Providers } from "@/providers/Providers";
+import "dotenv/config";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,11 +23,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
+      <Providers>
+        <body className={inter.className}>
+          <Header />
+          {children}
+          <Footer />
+        </body>
+      </Providers>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,28 @@
+"use client";
 import Article from "@/components/Article";
 import Contributors from "@/components/Contributors";
 import Recommendations from "@/components/Recommendations";
 import Search from "@/components/Search";
+import axios from "@/shared/axios";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["home"],
+    queryFn: () => axios.get("/"),
+  });
+
+  if (isPending) return <div className="h-screen"></div>
+
   return (
     <div className="m-5 gap-5 flex">
       <aside className="flex flex-col gap-7">
         <Search />
-        <Recommendations />
+        <Recommendations recommendations={data?.data.recomended} />
         <Contributors />
       </aside>
       <main className="w-max h-fit flex gap-3 justify-around flex-wrap ">
-        <Article />
+        <Article/>
       </main>
     </div>
   );
