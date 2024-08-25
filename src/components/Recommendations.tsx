76252a1fa41853a18c5a3d60fd41a6889ad1b7ef
Lucas-Winicius <lucasclucas7@gmail.com>
2024-08-24 21:56:05 -0300

    :sparkles: feat: Adding requests from the Recommendations component

--- src/components/Recommendations.tsx
+++ src/components/Recommendations.tsx
@@ -1,29 +1,29 @@
-interface RecommendationsType {
-  id: string;
-  image: string;
+"use client";
+import axios from "@/shared/axios";
+import { useQuery } from "@tanstack/react-query";
+
+type RecommendationsResponse = {
   title: string;
-  subtitle: string;
   slug: string;
-  content: string;
-  authorId: number;
-  createdAt: string;
-  updatedAt: string;
 }
 
-export default function Recommendations({
-  recommendations,
-}: {
-  recommendations: RecommendationsType[];
-}) {
+export default function Recommendations() {
+  const { isPending, isError, data: recommendations, error } = useQuery({
+    queryKey: ["recommendations"],
+    queryFn: () => axios.get<RecommendationsResponse[]>("/info/recommendations"),
+  });
+
+  if (isPending) return <div>Carregando Recomendações...</div>;
+
   return (
     <aside className="bg-zinc-100 px-4 py-6 space-y-5 rounded-lg w-72">
       <h1 className="font-bold text-lg">Recomendados</h1>
 
-      {recommendations.map((recommendation, id) => (
+      {recommendations?.data.map((recommendation, id) => (
         <a
           href={`/article/${recommendation.slug}`}
           className="flex items-center space-x-5"
-          key={recommendation.id}
+          key={recommendation.slug}
         >
           <h1 className="text-neutral-500 font-medium text-5xl">{++id}</h1>
           <p className="text-sm font-semibold">{recommendation.title}</p>
