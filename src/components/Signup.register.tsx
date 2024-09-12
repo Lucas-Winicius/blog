63ebf32f1f67f76ca73af689c35e731832049ce4
Lucas-Winicius <lucasclucas7@gmail.com>
2024-09-12 00:14:01 -0300

    :sparkles: feat: Adding zod and react forms to login

--- src/components/Signup.register.tsx
+++ src/components/Signup.register.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { z } from "zod";
 import { Button } from "./ui/button";
 import {
   Card,
@@ -10,8 +12,39 @@ import {
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { TabsContent } from "./ui/tabs";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const loginSchema = z.object({
+  username: z
+    .string()
+    .min(5, "Ops, verifique seu nome de usuario e tente novamente.")
+    .max(255, "Seu nome de usuario precisa ter no máximo 255 caracteres.")
+    .regex(
+      /^[a-zA-Z0-9._-]{1,}$/,
+      "Seu nome de usuario pode apenas conter caracteres alfanumericos ou ., _, -."
+    )
+    .regex(/^[^\W\d_].*/, "Seu nome de usuario não pode iniciar com simbolos."),
+
+  password: z
+    .string()
+    .min(8, "Algo está errado com sua senha tente novamente."),
+});
+
+type LoginSchema = z.infer<typeof loginSchema>;
 
 export default function Signup() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const handleLogin = (data: LoginSchema) => {
+    console.log(data);
+  };
   return (
     <TabsContent value="login">
       <Card>
@@ -20,18 +53,40 @@ export default function Signup() {
           <CardDescription>Seja Bem-Vindo(a), unknown!</CardDescription>
         </CardHeader>
         <CardContent className="space-y-2">
-          <div className="space-y-1">
-            <Label htmlFor="username">Nome de usuário</Label>
-            <Input id="username" placeholder="a.juninho123" />
-          </div>
-          <div className="space-y-1">
-            <Label htmlFor="password">Senha</Label>
-            <Input id="password" placeholder="*********" />
-          </div>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit(handleLogin)}
+          >
+            <div className="space-y-1">
+              <Label htmlFor="username">Nome de usuário</Label>
+              <Input
+                id="username"
+                placeholder="a.juninho123"
+                className={errors.username && "border border-red-600"}
+                {...register("username")}
+              />
+              <p className="text-xs text-red-600 font-semibold my-2">
+                {errors.username?.message}
+              </p>
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="password">Senha</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="*********"
+                className={errors.password && "border border-red-600"}
+                {...register("password")}
+              />
+              <p className="text-xs text-red-600 font-semibold my-2">
+                {errors.password?.message}
+              </p>
+            </div>
+            <Button type="submit" variant="default">
+              Entrar
+            </Button>
+          </form>
         </CardContent>
-        <CardFooter>
-          <Button variant="default">Entrar</Button>
-        </CardFooter>
       </Card>
     </TabsContent>
   );
