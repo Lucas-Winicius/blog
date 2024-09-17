bd76cc304fb74648848d80ed9bb88f8aac5467e3
Lucas-Winicius <lucasclucas7@gmail.com>
2024-09-16 21:50:18 -0300

    :lipstick: feat: Replacing error icons

--- src/components/Contributors.error.tsx
+++ src/components/Contributors.error.tsx
@@ -1,9 +1,9 @@
-import { TriangleAlert } from "lucide-react";
+import { UserRoundX } from "lucide-react";
 
 export default function ContributorsError() {
   return (
     <aside className="flex flex-col items-center gap-4 bg-zinc-100 px-4 py-6 rounded-lg w-72">
-      <TriangleAlert size={50} color="red" />
+      <UserRoundX size={50} color="red" />
       <p className="font-semibold text-center">
         Ooops! Não conseguimos carregar os contribuidores
       </p>
--- src/components/Recommendations.error.tsx
+++ src/components/Recommendations.error.tsx
@@ -1,9 +1,9 @@
-import { TriangleAlert } from "lucide-react";
+import { Bug } from "lucide-react";
 
 export default function RecommendationsError() {
   return (
     <aside className="flex flex-col items-center gap-4 bg-zinc-100 px-4 py-6 rounded-lg w-72">
-      <TriangleAlert size={50} color="red" />
+      <Bug size={50} color="red" />
       <p className="font-semibold text-center">
         Ooops! Não conseguimos carregar as recomendações
       </p>
